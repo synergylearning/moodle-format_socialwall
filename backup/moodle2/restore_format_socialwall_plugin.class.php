50c2df4ce9a67da73a65a954298638087a041e29
László Záborski <laszlo.zaborski@learningpool.com>
2021-02-17 20:19:33 +0000

    LMS-5090 format_socialwall: solve line exceeds maximum limit in the backup/moodle2/restore_format_socialwall_plugin.class.php.

--- backup/moodle2/restore_format_socialwall_plugin.class.php
+++ backup/moodle2/restore_format_socialwall_plugin.class.php
@@ -114,7 +114,8 @@ class restore_format_socialwall_plugin extends restore_local_plugin {
 
         $courseid = $this->task->get_courseid();
 
-        if (!$childcomments = $DB->get_records_select('format_socialwall_comments', " courseid = ? and replycommentid > 0", array($courseid))) {
+        if (!$childcomments = $DB->get_records_select('format_socialwall_comments', " courseid = ? and replycommentid > 0",
+                array($courseid))) {
             return;
         }
 
