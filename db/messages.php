495acee0068a9153b5beeab941e1cf92ec5c98cb
László Záborski <laszlo.zaborski@learningpool.com>
2021-02-18 15:53:44 +0000

    LMS-5090 format_socialwall: Add reuqired MOODLE_INTERNAL defines to db/messages.php.

--- db/messages.php
+++ db/messages.php
@@ -21,6 +21,9 @@
  * @copyright 2014 Andreas Wagner, Synergy Learning
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
 $messageproviders = array(
     'timelineposts' => array(
         'defaults' => array(
